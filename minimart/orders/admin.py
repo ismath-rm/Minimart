ed84115233f648e86aa5ca9bbee42c4cc26f6fe0
ismath <ismathrm9@.com>
2025-04-30 09:35:17 +0530

    update apps

--- minimart/orders/admin.py
+++ minimart/orders/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'date')
+    inlines = [OrderItemInline]
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'product', 'qty', 'discount', 'get_final_price')
